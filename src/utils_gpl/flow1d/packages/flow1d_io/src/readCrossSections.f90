25baafc267cb82884cf783bad98b0d5a8daf2e79
Jan Mooiman <Jan.Mooiman@deltares.nl>
2021-06-01 19:24:40 +0000

    Issue SOBEK3-2027: Just an error-message if "total area behind summer dike" is less than "flow area behind summer dike".

--- src/utils_gpl/flow1d/packages/flow1d_io/src/readCrossSections.f90
+++ src/utils_gpl/flow1d/packages/flow1d_io/src/readCrossSections.f90
@@ -653,7 +653,6 @@ module m_readCrossSections
       if (success) call prop_get_double(node_ptr, '', 'sd_totalArea', totalArea, success)
       if (success .and. flowArea > totalArea) then
             call SetMessage(LEVEL_ERROR, 'Total area behind summerdike should be larger then flow area behind summerdike. Cross-Section Definition id: '//trim(pCS%id)//'.')
-            return
       endif      
       if (success) then
          if (flowArea > 1.0d-5 .or. totalArea > 1.0d-5) then
