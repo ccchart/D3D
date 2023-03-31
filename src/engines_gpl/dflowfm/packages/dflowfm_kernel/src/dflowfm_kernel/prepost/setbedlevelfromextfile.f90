fde28f96b698f7002579782b7bec59a394e48364
Victor Chavarrias <chavarrias.v@gmail.com>
2023-03-31 16:34:04 +0200

    Update setbedlevelfromextfile.f90
    apply correction to the bed level rather than just mirroring. It contains ad-hoc hard-coded commands to be changed.

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/setbedlevelfromextfile.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/setbedlevelfromextfile.f90
@@ -43,8 +43,9 @@ subroutine setbedlevelfromextfile()    ! setbedlevels()  ! check presence of old
  use string_module, only: strcmpi
  use unstruc_inifields, only: readIniFieldProvider, checkIniFieldFileVersion
  use dfm_error
- 
  use unstruc_netcdf
+ use m_flowparameters, only: jadpsopt 
+ 
  implicit none
 
  logical, external :: timespaceinitialfield_mpi
@@ -73,6 +74,8 @@ subroutine setbedlevelfromextfile()    ! setbedlevels()  ! check presence of old
  character(len=255)              :: basedir
  integer :: i, iLocType
 
+ double precision :: dxz, dyz, bedslopex, bedslopey
+ 
  kc_size_store = 0
  inifield_ptr => null()
 
@@ -253,7 +256,39 @@ bft:do ibathyfiletype=1,2
     do L = Lnxi + 1, Lnx
         k1 = ln(1,L)
         k2 = ln(2,L)
-        bl(k1) = bl(k2)
+        bl(k1) = bl(k2) !original
+        if (jadpsopt.eq.2) then !this should be in a different flag
+            !we first copy and then add a correction to `bl(k1)`
+            dxz=xz(k1)-xz(k2)
+            dyz=yz(k1)-yz(k2)
+            !m_fm_erosed::e_dzdn check if this can be somehow used
+            bedslopex=-0.007d0/9.81d0 !AD-HOC make input or read from some existing variable.
+            bedslopey=0d0 !AD-HOC make input or read from some existing variable.
+            if (L.ne.101) then !downstream boundary (H-boundary) mega ad-hoc 1D case!!!!
+                !we want that at an H-boundary the bed level of the ghost cell is
+                !the correct one, as if the sloping bed would continue. In this 
+                !way, when computing the mean of the bed levels at cell centres,
+                !the elevation at the dowsntream link will be the correct one and
+                !`hu` will be correct if the water level at the cell centre of the 
+                !ghost cell (i.e., using `izbndpos=0` is `\Delta x` down from the 
+                !correct value. Note that it is `\Delta x` and not `\Delta x/2`.
+                bl(k1)=bl(k2)+bedslopex*dxz+bedslopey*dyz
+            else !upstream boundary (u-boundary)
+                !we want that at a u-boundary the bed level of the ghost cell is 
+                !the same as that of the second-inside cell. In this way, when 
+                !computing the mean of the bed levels at cell centres, the elevation
+                !at the upstream link will be the same as that of the second link. 
+                !As the water level of the first inside cell is (1) downwinded for
+                !computing `hu` at the second link and (2) copied to the upstream 
+                !ghost cell, we will obtain the same correct `hu` at the first link.
+                !
+                !For a case of a sloping bed in the positive x direction and a u-boundary
+                !imposed upstream, `dxz` will be negative upstream, hence obtaining the
+                !elevation of the second inside cell. 
+                bl(k1)=bl(k2)-bedslopex*dxz+bedslopey*dyz
+            endif
+        endif
+        
     enddo
     call mess(LEVEL_INFO, 'setbedlevelfromextfile: Mirroring input bedlevels at open boundaries.')
 
