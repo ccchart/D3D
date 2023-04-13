bb5ded9441bbb84bafa7fcf8df0bef05385b8621
buwalda <floris.buwalda@deltares.nl>
2023-04-13 15:27:29 +0200

    Added timesteps and timestep_sum array to statistical_output_item. Remove if-statement in update_moving_average since we initialize arrays to 0.

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/m_statistical_output.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/m_statistical_output.f90
@@ -1,54 +1,181 @@
+!!  Copyright (C)  Stichting Deltares, 2012-2023.
+!!
+!!  This program is free software: you can redistribute it and/or modify
+!!  it under the terms of the GNU General Public License version 3,
+!!  as published by the Free Software Foundation.
+!!
+!!  This program is distributed in the hope that it will be useful,
+!!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+!!  GNU General Public License for more details.
+!!
+!!  You should have received a copy of the GNU General Public License
+!!  along with this program. If not, see <http://www.gnu.org/licenses/>.
+!!
+!!  contact: delft3d.support@deltares.nl
+!!  Stichting Deltares
+!!  P.O. Box 177
+!!  2600 MH Delft, The Netherlands
+!!
+!!  All indications and logos of, and references to registered trademarks
+!!  of Stichting Deltares remain the property of Stichting Deltares. All
+!!  rights reserved.
+
+!> This module implements the statistical output in D-Flow FM.
 module m_statistical_output
+   use MessageHandling
+   use m_output_config
    
-   contains
+private
+
+   public realloc
+   public dealloc
+
+   !> Realloc memory cross-section definition or cross-sections
+   interface realloc
+      module procedure realloc_stat_output
+   end interface
+
+   !> Free the memory of cross-section definition or cross-sections
+   interface dealloc
+      module procedure dealloc_stat_output
+   end interface dealloc
+
+   integer, parameter :: SO_CURRENT = 1
+   integer, parameter :: SO_AVERAGE = 2
+   integer, parameter :: SO_MAX     = 3
+   integer, parameter :: SO_MIN     = 4
    
-subroutine update_moving_average(stat_item)
+   !> Derived type for the statistical output items. 
+   type, public :: t_output_variable_item
+      type(t_output_quantity_config), pointer   :: output_config        !< Pointer to output configuration item.
+      integer                                   :: operation_id         !< Specifies the kind of operation to perform on the output variable.
+      integer                                   :: current_step         !< Latest entry in the work array. MOD(current_step+1,total_steps_count) is the next 
+      integer                                   :: total_steps_count
+      !< item to remove.   
+      double precision, pointer, dimension(:)   :: stat_output          !< Array that is to be written to the Netcdf file. In case the current values are
+                                                                        !< required this variable points to the basic variable (e.g. s1).
+                                                                        !< Otherwise during the simulation the intermediate results are stored.
+      double precision, pointer, dimension(:)   :: stat_input           !< In case a statistical operation is requested. This pointer points to the
+                                                                        !< basic variable.
+      double precision, pointer, dimension(:,:) :: samples              !< In case a moving average is requested. This pointer points to the
+                                                                        !< work array, where the different samples are stored.
+      double precision, pointer, dimension(:)   :: moving_average_sum !< In case a moving average is requested. This pointer points to the
+                                                                        !< actual average values.
+      double precision                          :: timestep_sum         !< sum of timesteps (for moving average/ average calculation)
+      
+      double precision, pointer, dimension(:)   :: timesteps            !< array of timesteps belonging to samples in samples array
 
-type(t_statistical_output_item), intent(inout) :: stat_item
-integer :: inew, iold
+   end type t_output_variable_item
 
-inew = stat_item%current_step
-iold = MOD(stat_item%current_step+1,stat_item%total_steps_count)
+   !> Derived type to store the cross-section set
+   type, public :: t_output_variable_set
+      integer                                                :: size = 0                  !< Actual size of cross-section set
+      integer                                                :: growsby = 200             !< Increment for cross-section set
+      integer                                                :: count= 0                  !< Actual number of cross-section sets
+      type(t_output_variable_item), pointer, dimension(:)    :: statout                   !< Current cross-section
+   end type t_output_variable_set
 
-if (total_steps_count >= windowsize) then !only remove oldest sample if window is full
-   stat_item%moving_average = stat_item%moving_average - samples(:,iold)*timesteps(iold) + samples(:,inew)*timesteps(inew)
-   stat_item%timestepsum = stat_item%stimestep_sum - timesteps(iold) + timesteps(inew)
-else
-   stat_item%moving_average = stat_item%moving_average + samples(:,inew)*timesteps(inew)
-   stat_item%timestepsum = stat_item%stimestep_sum + timesteps(inew)
-endif
+contains
+
+   subroutine realloc_stat_output(statoutput)
+      ! Modules
+      use m_alloc
+
+      implicit none
+      ! Input/output parameters
+      type(t_output_variable_set), intent(inout)   :: statoutput !< Current cross-section definition
+      
+   
+      ! Local variables
+      integer                   :: ierr
+      type(t_output_variable_item), pointer, dimension(:)    :: oldstats
+   
+      ! Program code
+   
+      if (statoutput%size > 0) then
+         oldstats=>statoutput%statout
+      endif
+   
+      if (statoutput%growsBy <=0) then
+         statoutput%growsBy = 200
+      endif
+      allocate(statoutput%statout(statoutput%size+statoutput%growsBy),stat=ierr)
+      call aerr('statoutput%statout(statoutput%size+statoutput%growsBy)',ierr,statoutput%size+statoutput%growsBy)
+   
+      if (statoutput%size > 0) then
+         statoutput%statout(1:statoutput%size) = oldstats(1:statoutput%size)
+         deallocate(oldstats)
+      endif
+      statoutput%size = statoutput%size+statoutput%growsBy
+   end subroutine realloc_stat_output
+
+   subroutine dealloc_stat_output(statoutput)
+      implicit none
+      ! Input/output parameters
+      type(t_output_variable_set), intent(inout)   :: statoutput !< Current cross-section definition
+
+      if (statoutput%size> 0) then
+         deallocate(statoutput%statout)
+      endif
+   end subroutine dealloc_stat_output
+
+subroutine update_moving_average(i)
+
+type(t_output_variable_item), intent(inout) :: i
+integer :: jnew, jold !< Index to newest and oldest timestep in samples array
+
+jnew = i%current_step
+jold = MOD(i%current_step,i%total_steps_count)+1
+
+!when timestep < windowsize, no samples need to be removed. The timesteps array and samples array will be initialized to 0 so that we can keep the same expression.
+i%moving_average_sum = i%moving_average_sum - i%samples(:,jold)*i%timesteps(jold) + i%samples(:,jnew)*i%timesteps(jnew)
+i%timestep_sum = i%timestep_sum - timesteps(jold) + timesteps(jnew)
+
 
 end subroutine update_moving_average
 
-subroutine update_statistical_output(stat_item)
+subroutine update_statistical_output(i)
 
-if (stat_item%operation_id >= 2) then ! max/min of moving average requested
-   call update_moving_average(stat_item)
+type(t_output_variable_item), intent(inout) :: i
+
+if (i%operation_id >= 2) then ! max/min of moving average requested
+   call update_moving_average(i)
 endif
 
-select case (stat_item%operation_id)
+select case (i%operation_id)
 case (1) !SO_CURRENT
-   stat_item%output = stat_item%input
+   i%stat_output = i%stat_input
 case (2) !SO_AVERAGE
-   stat_item%output = stat_item%output + stat_item%input * stat_item%timesteps(stat_item%current_step)
-   stat_item%timestepsum = stat_item%timestepsum + stat_item%timesteps(stat_item%current_step)
+   i%stat_output = i%stat_output + i%stat_input * i%timesteps(i%current_step)
+   i%timestep_sum = i%timestep_sum + i%timesteps(i%current_step)
 case (3) !SO_MAX
-   stat_item%output = max(stat_item%output,stat_item%moving_average/stat_item%timestepsum)
+   i%stat_output = max(i%stat_output,i%moving_average_sum/i%timestep_sum)
 case (4) !SO_MIN
-   stat_item%output = min(stat_item%output,stat_item%moving_average/stat_item%timestepsum)
+   i%stat_output = min(i%stat_output,i%moving_average_sum/i%timestep_sum)
 end select
 
 end subroutine update_statistical_output
 
-subroutine write_statistical_output(stat_item)
+subroutine prepare_write_statistical_output(i)
 
-if (stat_item%operation_id == 2)
-   stat_item%output = stat_item%output/stat_item%timestepsum
+type(t_output_variable_item), intent(inout) :: i
+
+if (i%operation_id == 2) then !SO_AVERAGE
+   i%stat_output = i%stat_output/i%timestep_sum
 endif
 
-!call netcdf_write_his/map/else (stat_output_item%output)
+end subroutine prepare_write_statistical_output
 
-end subroutine update_statistical_output
-   
+subroutine reset_statistical_output(i)
+
+type(t_output_variable_item), intent(inout) :: i
+
+i%stat_output = 0
+if (i%operation_id == 2) then !SO_AVERAGE
+   i%timestep_sum = 0 
+endif
+
+end subroutine reset_statistical_output
+
 end module
-   
