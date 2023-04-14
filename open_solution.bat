e9136e77c7837a79db7d2aa6075ba095cc776940
V <victor.chavarrias@deltares.nl>
2023-04-14 14:45:44 +0200

    Create open_solution.bat
    add to solution. then we have to remove it.

---
+++ open_solution.bat
@@ -0,0 +1,3 @@
+set tmp=c:\checkouts\tmp_01
+set temp=c:\checkouts\tmp_02
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe" "c:\checkouts\delft3d\build_all\all.sln"
