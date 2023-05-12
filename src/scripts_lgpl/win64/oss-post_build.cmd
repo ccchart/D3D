100dfe3afd3ba9f9d29cb8d72066ff4943f2c9d8
buwalda <floris.buwalda@deltares.nl>
2023-05-12 14:18:03 +0200

    restored post build dfmoutput target

--- src/scripts_lgpl/win64/oss-post_build.cmd
+++ src/scripts_lgpl/win64/oss-post_build.cmd
@@ -819,7 +819,7 @@ rem ==========================
     if "%configuration%" == "Debug" (
 
         echo "Debug postbuild"
-        set dest_bin=    "!install_dir!\x64\Debug\"
+        set dest_bin=	 "%install_dir%\x64\Debug"
         set dest_default="!install_dir!\x64\Debug\"
         set dest_scripts="!install_dir!\x64\Debug\"
         set dest_plugins="!install_dir!\x64\Debug\"
